77aa48841d9370ae31bad533559e7c0fa6214039
Rishabh10001 <rishabh.j1910@gmail.com>
2025-03-23 10:24:09 +0530

    added the pipeline

--- __main__.py
+++ __main__.py
@@ -1,10 +1,30 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
+import pulumi_eks as eks
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+# Create a new VPC
+vpc = aws.ec2.Vpc("my-vpc",
+    cidr_block="10.0.0.0/16",
+    enable_dns_support=True,
+    enable_dns_hostnames=True
+)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Create a subnet
+subnet = aws.ec2.Subnet("my-subnet",
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    map_public_ip_on_launch=True
+)
+
+# Create an EKS Cluster
+cluster = eks.Cluster("my-eks-cluster",
+    vpc_id=vpc.id,
+    public_subnet_ids=[subnet.id],  # Public subnet for free-tier instances
+    instance_type="t3.micro",  # Free-tier eligible
+    desired_capacity=1,
+    min_size=1,
+    max_size=2
+)
+
+# Export the cluster kubeconfig
+pulumi.export("kubeconfig", cluster.kubeconfig)
