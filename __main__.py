647e0808136103582f89eb260fb81de1b307cbd1
Rishabh10001 <rishabh.j1910@gmail.com>
2025-03-23 10:36:44 +0530

    changed the main.py

--- __main__.py
+++ __main__.py
@@ -2,6 +2,12 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_eks as eks
 
+# Get the default AWS region
+aws_region = aws.get_region().name
+
+# Fetch available AZs in the region
+azs = aws.get_availability_zones()
+
 # Create a new VPC
 vpc = aws.ec2.Vpc("my-vpc",
     cidr_block="10.0.0.0/16",
@@ -9,17 +15,25 @@ vpc = aws.ec2.Vpc("my-vpc",
     enable_dns_hostnames=True
 )
 
-# Create a subnet
-subnet = aws.ec2.Subnet("my-subnet",
+# Create two subnets in different Availability Zones
+subnet1 = aws.ec2.Subnet("subnet-1",
     vpc_id=vpc.id,
     cidr_block="10.0.1.0/24",
+    availability_zone=azs.names[0],  # First AZ
+    map_public_ip_on_launch=True
+)
+
+subnet2 = aws.ec2.Subnet("subnet-2",
+    vpc_id=vpc.id,
+    cidr_block="10.0.2.0/24",
+    availability_zone=azs.names[1],  # Second AZ
     map_public_ip_on_launch=True
 )
 
 # Create an EKS Cluster
 cluster = eks.Cluster("my-eks-cluster",
     vpc_id=vpc.id,
-    public_subnet_ids=[subnet.id],  # Public subnet for free-tier instances
+    public_subnet_ids=[subnet1.id, subnet2.id],  # ✅ Using two subnets in different AZs
     instance_type="t3.micro",  # Free-tier eligible
     desired_capacity=1,
     min_size=1,
